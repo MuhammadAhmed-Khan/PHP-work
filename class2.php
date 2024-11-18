bd07afd9ba375866eb0c757b60abefe01f2ce7b7
MuhammadAhmed-Khan <studyace56@gmail.com>
2024-11-18 12:16:37 +0500

    class2commint

---
+++ class2.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // print Method  
+    // echo "Muhammad Ahmed Khan";
+
+    // print "Muhammad Ahmed khan";
+
+    // creating a variable
+    // $name = "Sir Ahmed";
+    // echo $name;
+
+
+    // $age = 22;
+    // echo $age;
+
+    // concat
+    // $age =  22;
+    // echo "The Age is " .$age;
+
+    // datatypes In Php
+    // String "",''
+    // int    2454
+    // boolean  true 1 or false 0
+    // null null
+    // array ["name"]
+    // object {"ali","Ahmed"}
+    // float 45.54
+    // undefined
+
+    $name;
+    echo $name;
+
+
+        ?>
+
+    
+</body>
+</html>
